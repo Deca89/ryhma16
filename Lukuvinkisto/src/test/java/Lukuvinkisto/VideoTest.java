38b1ec529cc76c996cb15bd4aee46c7cc0fb831f
vornsami <sami.vornanen@helsinki.fi>
2020-12-10 16:31:04 +0200

    Testikorjaukset

--- Lukuvinkisto/src/test/java/Lukuvinkisto/VideoTest.java
+++ Lukuvinkisto/src/test/java/Lukuvinkisto/VideoTest.java
@@ -35,12 +35,12 @@ public class VideoTest {
         videos.add(new Video("Stephen Moore reads The Hitch-Hiker's Guide to the Galaxy by Douglas Adams", "https://youtu.be/dPbr0v_V-cI", List.of("ab", "cd")));
         videos.add(new Video("The Hitchhiker's Guide to the Galaxy read by Douglas Adams [Part 1 of 4]", "https://youtu.be/FmakHVY7xeU", List.of("ef", "gh")));
         Collections.sort(videos);
-        assertEquals(videos.get(0).toString(), "Stephen Moore reads The Hitch-Hiker's Guide to the Galaxy by Douglas Adams : https://youtu.be/dPbr0v_V-cI, Tagit: ab, cd; Katsottu");
+        assertEquals(videos.get(0).toString(), "Stephen Moore reads The Hitch-Hiker's Guide to the Galaxy by Douglas Adams : https://youtu.be/dPbr0v_V-cI, Tagit: ab, cd; Katsomatta");
     }
     @Test
     public void ReturnCorrectListElement() {
         Video video = new Video(1, "Stephen Moore reads The Hitch-Hiker's Guide to the Galaxy by Douglas Adams", "https://youtu.be/dPbr0v_V-cI", List.of("ab", "cd"));
-        assertEquals(video.getAsListElement(), "<a href=\"https://youtu.be/dPbr0v_V-cI\">Stephen Moore reads The Hitch-Hiker's Guide to the Galaxy by Douglas Adams<a>, Tagit: ab, cd; Katsottu <form method=\"GET\" action=\"/video/1\">\n" +
+        assertEquals(video.getAsListElement(), "<a href=\"https://youtu.be/dPbr0v_V-cI\">Stephen Moore reads The Hitch-Hiker's Guide to the Galaxy by Douglas Adams<a>, Tagit: ab, cd; Katsomatta <form method=\"GET\" action=\"/video/1\">\n" +
                 "        <input type=\"submit\" name=\"muokkaavideo\" value=\"Muokkaa\" />\n" +
                   "</form>");
     }
